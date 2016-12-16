fe75408272b123054c430b74f095d6c10b9bf452
Jeff Ducharme <jeffreywyattducharme@gmail.com>
2016-12-15 17:29:09 -0800

    added click handler, and responsiveness

--- js/sounds.js
+++ js/sounds.js
@@ -1,27 +1,26 @@
-/*jshint devel:true */
-
 function playAudio (e) {
-	var $audio = $('audio[data-key='+ e.originalEvent.keyCode + ']')[0];
-	var $key = $('.key[data-key='+ e.originalEvent.keyCode + ']');
+	var identifier = e.target.dataset.key || e.originalEvent.keyCode;
+	
+	var $audio = $('audio[data-key='+ identifier + ']')[0];
+	var $key = $('.key[data-key='+ identifier + ']');
+
 	if(!$audio){
 		return;
 	}
 	$audio.currentTime = 0;
 	$audio.play();
-	$key.addClass('playing');
-	
+	$key.addClass('playing');	
 }
 
 function removeTransitionClass(e) {
 	if(e.originalEvent.propertyName !== 'transform'){
 		return;
 	}
-	console.log($(this).text());
 	$(e.target).removeClass('playing');
 }
 
 $(document).ready(function(){
 	$('.keys').bind( 'transitionend', removeTransitionClass);
 	$(window).keydown( playAudio );
-
+	$('.keys').click( playAudio );  
 });
